1e9a955aa1eff0571ecd263c95ece798aa8120ad
fazerog02 <fazerog02@gmail.com>
2023-11-21 10:25:28 +0900

    [add]06/02

--- 06/kadai01-04.r
+++ 06/kadai01-04.r
@@ -27,62 +27,70 @@ print(p)
 # p = 0.1316 > α = 0.05 なので帰無仮説は棄却されず，AとBに有意な差があるとは言えない．
 
 png("kadai06-02.png", width = 512, height = 512)  # グラフを画像で保存する
-# hist(raw_data$A, breaks = 15, xlab = "value", ylab = "frequency", main = "Group A of data_r.txt")  # Aのデータでヒストグラムを描画
-# par(new = TRUE)  # グラフの重ね合わせを許可
-# points(diff, y_center, pch = 16, col = "red", cex = 1.5)  # 実測値を点で描画
+curve(dnorm(x, mean = mean(diff_r), sd = sd(diff_r)), from = -1.5, to = 2.5, xlab = "x", ylab = "dnorm(x)", main = "dnorm with actual value")  # 理論分布を描画
+par(new = TRUE)  # グラフの重ね合わせを許可
+arrows(diff, 0.2, diff, 0, col = "blue", lwd = 1.5)  # 実測値を点で描画
 dev.off()  # 画像保存
 
 # (3)
-ps <- 0
-mean_p <- 0
-sd_p <- 0
-n_p <- 0
+# ps <- 0
+# mean_p <- 0
+# sd_p <- 0
+# n_p <- 0
 
-r <- 10
-n <- 25
-m <- 0
+# r <- 10
+# n <- 25
+# m <- 0
 
-while (n < 50000) {
-  n <- n * 2
-  m <- m + 1
-  for (j in 1:r) {
-    diff_r <- 0
-    for(i in 1:n) {
-      data_r <- sample(data, replace = FALSE)
-      a_r <- data_r[seq_along(raw_data$A)]
-      b_r <- data_r[(length(raw_data$A) + 1):length(data_r)]
-      diff_r[i] <- (mean(a_r) - mean(b_r))
-    }
-    diff_r[n + 1] <- diff
-    ps[j] <- sum(diff_r >= diff) / (n + 1)
-    mean_p[m] <- mean(ps)
-    sd_p[m] <- sd(ps)
-    n_p[m] <- n
-  }
-  print(n)
-}
-# 各種値の表示
-cat("m: ")
-print(m)
-cat("ps: ")
-print(ps)
-cat("mean_p: ")
-print(mean_p)
-cat("sd_p: ")
-print(sd_p)
-cat("n_p: ")
-print(n_p)
-png("kadai06-03.png", width = 512, height = 512)  # グラフを画像で保存する
-matplot(n_p, cbind(mean_p, sd_p), type = "l", col = c("blue", "red"), lwd = 2, xlab = "N", ylab = "value", lty = 1)
-legend("topleft", legend = c("Mean", "SD"), col = c("blue", "red"), lwd = 2, lty = 1)
-title("Mean and SD of p-value")
-dev.off()  # 画像を保存
+# while (n < 50000) {
+#   n <- n * 2
+#   m <- m + 1
+#   for (j in 1:r) {
+#     diff_r <- 0
+#     for(i in 1:n) {
+#       data_r <- sample(data, replace = FALSE)
+#       a_r <- data_r[seq_along(raw_data$A)]
+#       b_r <- data_r[(length(raw_data$A) + 1):length(data_r)]
+#       diff_r[i] <- (mean(a_r) - mean(b_r))
+#     }
+#     diff_r[n + 1] <- diff
+#     ps[j] <- sum(diff_r >= diff) / (n + 1)
+#     mean_p[m] <- mean(ps)
+#     sd_p[m] <- sd(ps)
+#     n_p[m] <- n
+#   }
+#   print(n)
+# }
+# # 各種値の表示
+# cat("m: ")
+# print(m)
+# cat("ps: ")
+# print(ps)
+# cat("mean_p: ")
+# print(mean_p)
+# cat("sd_p: ")
+# print(sd_p)
+# cat("n_p: ")
+# print(n_p)
+# png("kadai06-03.png", width = 512, height = 512)  # グラフを画像で保存する
+# matplot(n_p, cbind(mean_p, sd_p), type = "l", col = c("blue", "red"), lwd = 2, xlab = "N", ylab = "value", lty = 1)
+# legend("topleft", legend = c("Mean", "SD"), col = c("blue", "red"), lwd = 2, lty = 1)
+# title("Mean and SD of p-value")
+# dev.off()  # 画像を保存
+# m: [1] 11
+# ps:  [1] 0.06759634 0.06876819 0.06607293 0.06841663 0.06785024 0.06874866
+#  [7] 0.06763540 0.06636589 0.06630730 0.06685416
+# mean_p:  [1] 0.07450980 0.07722772 0.07412935 0.07356608 0.06803995 0.06926921
+#  [7] 0.06754139 0.06775504 0.06828373 0.06638413 0.06746157
+# sd_p:  [1] 0.034337060 0.017955737 0.013765510 0.008578457 0.010839797 0.005840462
+#  [7] 0.002702677 0.003926794 0.002438970 0.001082685 0.001016833
+# n_p:  [1]    50   100   200   400   800  1600  3200  6400 12800 25600 51200
 # n >= 12800からあまり値が変化していないので，n = 10000程度が好ましい
 
 # (4)
-library(perm)
-perm_result <- permTS(raw_data$A, raw_data$B, alternative = c("greater"))
-print(perm_result)
+library(perm)  # permを使用する
+perm_result <- permTS(raw_data$A, raw_data$B, alternative = c("greater"))  # ランダマイゼーション検定をする
+print(perm_result)  # 結果を表示
 
 #        Permutation Test using Asymptotic Approximation
 
@@ -93,8 +101,8 @@ print(perm_result)
 # mean raw_data$A - mean raw_data$B
 #                          1.075268
 
-t_result <- t.test(raw_data$A, raw_data$B, equal.var = TRUE, alternative = "g")
-print(t_result)
+t_result <- t.test(raw_data$A, raw_data$B, equal.var = TRUE, alternative = "g")  # t検定をする
+print(t_result)  # 結果を表示
 
 #        Welch Two Sample t-test
 
